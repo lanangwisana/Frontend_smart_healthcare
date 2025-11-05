6239cb76b4df2487062a2c3e21539e09e95b35b9
lanangwisana <lanangwisana@gmail.com>
2025-11-05 19:43:49 +0700

    remove tailwind vite

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('recources/css/app.css')
     <title>Document</title>
 </head>
 <body>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    
 </body>
 </html>
