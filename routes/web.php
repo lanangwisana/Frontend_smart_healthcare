6afc5e2e4b431c6c1374f63e05409b3045999aca
lanangwisana <lanangwisana@gmail.com>
2025-11-07 18:59:29 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\FrontAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/index', function () {
-    return view('index');
-});
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [FrontAuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [FrontAuthController::class, 'login']);
+
+Route::get('/register', [FrontAuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [FrontAuthController::class, 'register']);
